ccbf843bf7ef984ec4920d7a62bb2d9ee6c0522e
velykitty <jiyunee13@gmail.com>
2021-07-19 18:53:17 +0900

    set routes for auth api

---
+++ blog-server/src/api/auth/auth.ctrl.js
@@ -0,0 +1,15 @@
+export const register = async ctx => {
+    //register
+};
+
+export const login = async ctx => {
+    //login
+};
+
+export const check = async ctx => {
+    //check status
+};
+
+export const logout = async ctx => {
+    //logout
+};
