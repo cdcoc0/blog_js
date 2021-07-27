c10074592673a166687b79faf223ef4e0681a4ef
velykitty <jiyunee13@gmail.com>
2021-07-27 20:37:05 +0900

    tagbox ui

--- blog-client/src/pages/WritePage.js
+++ blog-client/src/pages/WritePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 
 const WritePage = () => {
     return (
         <>
+            <Responsive>카테고리</Responsive>
             <Editor />
+            <Responsive>
+                <TagBox />
+            </Responsive>
         </>
     );
 };
