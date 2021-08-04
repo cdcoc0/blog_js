7fa9c5ae37759b10c0cc01b8d0e812985be7eaff
velykitty <jiyunee13@gmail.com>
2021-08-05 01:43:56 +0900

    set original post for update

--- blog-client/src/modules/write.js
+++ blog-client/src/modules/write.js
@@ -4,12 +4,14 @@ import createRequestThunk, { createRequestActionTypes } from "../lib/createReque
 
 const INITIALIZE = 'write/INITIALIZE'; //모든 내용 초기화
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; //특정 key값 바꾸기
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
 
 const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE] = createRequestActionTypes('write/WRITE_POST');
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({key, value}));
 export const writePost = createRequestThunk(WRITE_POST, postsAPI.writePost);
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
 
 const initialState = {
     title: '',
@@ -17,6 +19,7 @@ const initialState = {
     tags: [],
     post: null,
     postError: null,
+    originalPostId: null,
 };
 
 const write = handleActions(
@@ -38,6 +41,13 @@ const write = handleActions(
         [WRITE_POST_FAILURE]: (state, {payload: postError}) => ({
             ...state,
             postError
+        }),
+        [SET_ORIGINAL_POST]: (state, {payload: post}) => ({
+            ...state,
+            title: post.title,
+            body: post.body,
+            tags: post.tags,
+            originalPostId: post._id
         })
     },
     initialState
